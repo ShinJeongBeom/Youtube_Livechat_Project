78da3cedf5ef651c3d40fda6e5b2515029f4084d
ShinJeongBeom <dingfan36@gmail.com>
2026-05-11 06:20:30 +0900

    Initial commit

---
+++ src/main/java/com/jeongbeom/youtube_livechat_project/YoutubeLivechatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jeongbeom.youtube_livechat_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeLivechatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeLivechatProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeongbeom/youtube_livechat_project/YoutubeLivechatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongbeom.youtube_livechat_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeLivechatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
